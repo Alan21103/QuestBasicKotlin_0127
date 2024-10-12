f5ea371f3603bdcf6ffd3bf8e5f99001a990453b
Alan21103 <syahlansaputra03@gmail.com>
2024-10-12 22:53:18 +0700

    Membuat data perulangan

--- app/src/main/java/com/example/praktikum1/ConditionalExpression.kt
+++ app/src/main/java/com/example/praktikum1/ConditionalExpression.kt
@@ -7,4 +7,22 @@ fun Perulangan() {
     for (i in 1..5) {
         println("Perulangan ke-$i")
     }
+
+    val cars = listOf("Bmw", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do{
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
 }
+
