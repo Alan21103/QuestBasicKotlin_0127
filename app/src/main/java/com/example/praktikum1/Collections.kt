6edc5c5ba97feaa9198fe110051a7379fdce7755
Alan21103 <syahlansaputra03@gmail.com>
2024-10-12 22:33:29 +0700

    Membuat Fungsi ContohMap Read-Only dan Map Mutable

--- app/src/main/java/com/example/praktikum1/Collections.kt
+++ app/src/main/java/com/example/praktikum1/Collections.kt
@@ -65,3 +65,22 @@ fun ContohSet() {
     val shapesLocked: Set<String> = shape
     println(shapesLocked)
 }
+
+// Map adalah Kumpulan data yang disusun dalam pasangan key-value
+// Map Bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// Map bersidat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Map Read-Only menggunakan mapOf
+// Map Mutable menggunakan mutableMapOf
+
+fun ContohMap() {
+
+    println()
+    println("=== Map ===")
+    // Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+}
