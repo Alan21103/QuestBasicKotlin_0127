b5a10c1e97bb22d8aa37a0dab1dfdbb3a2221aca
Alan21103 <syahlansaputra03@gmail.com>
2024-10-12 22:34:50 +0700

    Membuat Fungsi menambahkan data kedalam map mutable dan menghapus data dari map mutable

--- app/src/main/java/com/example/praktikum1/Collections.kt
+++ app/src/main/java/com/example/praktikum1/Collections.kt
@@ -83,4 +83,12 @@ fun ContohMap() {
     // Map Mutable
     val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(shape)
+
+    // Menambahkan data ke dalam map mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
 }
